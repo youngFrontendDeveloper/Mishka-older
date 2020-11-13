783e319bf0cab3377ec940adb385e12ca44b3216
Нина <young_imposer@mail.ru>
2020-11-13 20:08:00 +0300

    Сверстала страницу catalog

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,8 @@ gulp.task("copy", function () {
       "source/fonts/**/*.{woff,woff2}",
       "source/img/**",
       "source/js/**",
-      "source/*.ico"
+      "source/*.ico",
+      "source/video/*.*"
     ], {
       base: "source"
     })
